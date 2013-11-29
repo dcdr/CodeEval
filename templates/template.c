94ed3bc164ba9afd3b2cb5e9097dce38485c3073
Troy Bergstrand <troy.bergstrand@gmail.com>
2013-11-29 12:36:55 -0700

    Clean up C template

--- templates/template.c
+++ templates/template.c
@@ -7,28 +7,31 @@ int main(int argc, char **argv)
     FILE *f;
     char line[LINE_SIZE], *p;
 
-    // Open file passed as argument
+    // Open file passed as argument.
     if (argc < 2 || !(f = fopen(argv[1], "r"))) {
         fprintf(stderr, "Unable to open file argument\n");
         return 1;
     }
 
-    // Read lines from file
+    // Read lines from file.
     while (fgets(line, LINE_SIZE, f)) {
-        // Remove the trailing '\n'
+        // Remove the trailing '\n'.
         if ((p = strchr(line, '\n'))) { *p = '\0'; }
 
-        // Skip empty lines
+        // Skip empty lines.
         if (line[0] == '\0') { continue; }
 
         // Do something with the line
         printf("%s\n", line);
     }
 
-    // Paranoid check
+    // Paranoid check.
     if (ferror(f)) {
         perror("I/O Error");
     }
+
+    // Clean up.
+    fclose(f);
 
     return 0;
 }
