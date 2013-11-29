bba3034a6c325554b329cefd0d5db3399895ad4f
Troy Bergstrand <troy.bergstrand@gmail.com>
2013-11-29 13:07:08 -0700

    clean up

--- easy/unique elements/javascript/unique.js
+++ easy/unique elements/javascript/unique.js
@@ -8,8 +8,7 @@ var fs = require('fs');
 // Get all tests, one per line, skipping empty lines.
 var tests = fs.readFileSync(process.argv[2]).toString().split('\n').filter(function(line) {
 	return "" !== line;
-}).
-forEach(function(test) {				// For each test.
+}).forEach(function(test) {				// For each test.
 	var orig = test.split(',');			// Find all elements in the set.
 	var orig_len = orig.length;			// Find the size of the set.
 	var uniq = [];						// Resulting unique elements.
